c8c76617deb7fdd818cc57c51fec69fa5294fd1c
RJiazhen <103310338+RJiazhen@users.noreply.github.com>
2022-09-04 05:52:20 +0800

    refactor 删除已git的编译后文件和重新git manifest.json

--- unpackage/dist/dev/mp-weixin/store/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var common_vendor = require("../common/vendor.js");
-var store_sakura_arms = require("./sakura_arms.js");
-const store = common_vendor.createStore({
-  modules: {
-    m_sa: store_sakura_arms.moduleSakuraArms
-  }
-});
-exports.store = store;
