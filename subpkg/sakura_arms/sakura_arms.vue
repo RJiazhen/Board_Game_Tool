b3c0fe45b496e136ebfac77564d45b8aec00430f
RJiazhen <103310338+RJiazhen@users.noreply.github.com>
2022-09-09 11:02:52 +0800

    feat 添加使用帮助在首次启动时显示功能

--- subpkg/sakura_arms/sakura_arms.vue
+++ subpkg/sakura_arms/sakura_arms.vue
@@ -3,8 +3,11 @@
     <!-- 使用说明弹窗 -->
     <uni-popup ref="popup" type="top" mask-background-color="rgba(35, 35, 35, 0.8)">
       <view class="help-container">
-
         <view class="help">
+          <view><text class="bold" style="font-size: 20px;">【长按重置按钮</text>
+            <uni-icons class="reset-icon" type="loop" size="25px" color="#fff" /><text class="bold"
+              style="font-size: 20px;">可再次查看本帮助】</text>
+          </view>
           <view><text class="bold">1. 基本动作</text><text>：直接点击对应按钮</text></view>
           <view><text class="bold">2. 移动token</text><text>：先点击来源区域，再点击目标区域</text></view>
           <view><text class="bold">3. 打出付与牌</text><text>：点击“打出付与牌”的＋号按钮，再点击虚或装区域移动token，最后再点击 √ 确认完成</text></view>
@@ -13,10 +16,6 @@
 
             <uni-icons class="reset-icon" type="loop" size="25px" color="#fff" />，<text>并再次确认
             </text>
-          </view>
-
-          <view><text> 再次查看本帮助：长按重置按钮</text>
-            <uni-icons class="reset-icon" type="loop" size="25px" color="#fff" />
           </view>
           <view class="feedback">
             <text>如有问题和建议可以点击下方按钮进行反馈</text>
@@ -51,14 +50,15 @@
   } from 'vuex'
   export default {
     data() {
-      return {};
+      return {
+        toShowHelp: true
+      };
     },
     computed: {},
     methods: {
       ...mapMutations('m_sa', ['resetState', 'getFromStorage']),
       // 显示帮助
       showHelp() {
-        console.log('show');
         this.$refs.popup.open()
       },
       // 重置相关数值
@@ -75,7 +75,17 @@
     },
     // 加载时从本地存储获取数据
     onLoad() {
+      // 获取游戏面板数据
       this.getFromStorage()
+      // 获取是否显示帮助参数
+      this.toShowHelp = uni.getStorageSync('sa_toShowHelp').length != 0 ?
+        JSON.parse(uni.getStorageSync('sa_toShowHelp')) :
+        this.toShowHelp
+      // 判断是否显示帮助
+      if (this.toShowHelp) {
+        this.showHelp()
+        uni.setStorageSync('sa_toShowHelp', JSON.stringify(false))
+      }
     }
   }
 </script>
