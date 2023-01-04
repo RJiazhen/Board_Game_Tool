51c54332178a38ed1473b92312442bd791b63efa
RJiazhen <103310338+RJiazhen@users.noreply.github.com>
2023-01-04 21:10:06 +0800

    feat: 添加token提示组件

---
+++ components/sa-token-tip/sa-token-tip.vue
@@ -0,0 +1,20 @@
+<template>
+  <view>
+
+  </view>
+</template>
+
+<script>
+  export default {
+    name:"sa-token-tip",
+    data() {
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
