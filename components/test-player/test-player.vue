38bac73062719c303282f1ccc080895f951d4e26
RJiazhen <jiazhen.ruan@outlook.com>
2023-01-05 22:13:39 +0800

    style: 格式化player组件文件以及修改部分样式

--- components/test-player/test-player.vue
+++ components/test-player/test-player.vue
@@ -1,25 +1,25 @@
 <template>
-  <view class="player">
-    <div class="board board-1">
-      <view class="shared">
-        <!-- 虚 -->
-        <sa-shared-area name="shadow" style="width: 50%"></sa-shared-area>
-        <!-- 距 -->
-        <sa-shared-area name="distance" style="width: 50%"></sa-shared-area>
-      </view>
-      <!-- 樱花数量提示区域 -->
-      <sa-token-tip></sa-token-tip>
-      <!-- 个人状态区 -->
-      <view class="personal">
-        <!-- 装 -->
-        <sa-personal-area name="aura" style="width: 33%"></sa-personal-area>
-        <!-- 气 -->
-        <sa-personal-area name="aura" style="width: 33%"></sa-personal-area>
-        <!-- 命 -->
-        <sa-personal-area name="aura" style="width: 33%"></sa-personal-area>
-      </view>
-    </div>
-  </view>
+    <view class="player">
+        <div class="board board-1">
+            <view class="shared">
+                <!-- 虚 -->
+                <sa-shared-area name="shadow" style="width: 50%"></sa-shared-area>
+                <!-- 距 -->
+                <sa-shared-area name="distance" style="width: 50%"></sa-shared-area>
+            </view>
+            <!-- 樱花数量提示区域 -->
+            <sa-token-tip></sa-token-tip>
+            <!-- 个人状态区 -->
+            <view class="personal">
+                <!-- 装 -->
+                <sa-personal-area name="aura" style="width: 33%"></sa-personal-area>
+                <!-- 气 -->
+                <sa-personal-area name="aura" style="width: 33%"></sa-personal-area>
+                <!-- 命 -->
+                <sa-personal-area name="aura" style="width: 33%"></sa-personal-area>
+            </view>
+        </div>
+    </view>
 </template>
 
 <script setup lang="ts">
@@ -27,31 +27,33 @@
 </script>
 
 <style scoped lang="scss">
-  .player {
-    // height: 46.6vh;
-    width: 100vw;
-    padding: 0px 10px 0;
-    overflow: hidden;
+    .player {
+        // height: 46.6vh;
+        width: 100vw;
+        padding: 0px 10px 0;
+        overflow: hidden;
 
-    .board {
+        .board {
 
-      .shared {
-        height: 20.33vh;
-        display: flex;
+            .shared {
+                height: 20.33vh;
+                display: flex;
 
-        sa-shared-area:nth-child(1) {
-          margin-right: 3px;
+                sa-shared-area:nth-child(1) {
+                    margin-right: 3px;
+                }
+            }
+
+            .personal {
+                height: 23.92vh;
+                display: flex;
+
+                margin-top: 3px;
+
+                sa-personal-area:nth-child(-n+2) {
+                    margin-right: 3px;
+                }
+            }
         }
-      }
-
-      .personal {
-        height: 23.92vh;
-        display: flex;
-
-        sa-personal-area:nth-child(1) {
-          margin-right: 3px;
-        }
-      }
     }
-  }
 </style>
