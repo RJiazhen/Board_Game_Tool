8d670ebdaf19403d47ebb27fffc1f400a08e5659
RJiazhen <jiazhen.ruan@outlook.com>
2023-01-06 21:06:01 +0800

    feat: 完成中心菜单的基本布局和样式

---
+++ components/sa-menu/sa-menu.vue
@@ -0,0 +1,303 @@
+<template>
+    <view class="menu">
+
+        <!-- 环型菜单 -->
+        <view class="circle-menu" :class="isActive?'activated':'unactivated'">
+            <!-- 菜单边框 -->
+            <view class="menu-border"> </view>
+            <view class="btn-wheel">
+                <view class="btn" v-for="(btn, index) in btnList" :key="index" @click="btn.func()">
+                    <image class="btn-icon" :src="btn.icon" mode="aspectFit"></image>
+                    <text class="btn-name">{{btn.name}}</text>
+                </view>
+            </view>
+        </view>
+        <!-- 中心按钮 -->
+        <view class="center-btn" :class="isActive?'activated':'unactivated'" @click="changeActive">
+            <!-- 按钮背景 -->
+            <view class="center-btn-bg"> </view>
+            <!-- 打开按钮 -->
+            <view class="btn-open">
+                <view class="rect"> </view>
+                <view class="rect"> </view>
+                <view class="rect"> </view>
+            </view>
+            <!-- 关闭按钮 -->
+            <view class="btn-close">
+                <view class="rect"></view>
+                <view class="rect"></view>
+            </view>
+        </view>
+    </view>
+</template>
+
+<script setup lang="ts">
+    import {
+        ref
+    } from "vue";
+
+    // 当前激活状态
+    const isActive = ref(true)
+
+    // 改变激活状态
+    const changeActive = () => {
+        isActive.value = !isActive.value
+    }
+
+    // 环型菜单列表
+    const btnList = [{
+            name: '重置',
+            icon: '../../static/sakura_arms/reset_icon.svg',
+            func: () => {
+                console.log('reset');
+            }
+        },
+        {
+            name: '重置',
+            icon: '../../static/sakura_arms/reset_icon.svg',
+            func: () => {
+                console.log('reset');
+            }
+        }
+    ]
+</script>
+
+<style scoped lang="scss">
+    .menu {
+        position: relative;
+
+        // 环型菜单
+        .circle-menu {
+
+            &.unactivated {
+                width: 0;
+                height: 0;
+                position: absolute;
+                left: 50%;
+                top: 50%;
+
+                // 外圈边框
+                .menu-border {
+                    display: none;
+                    width: 0;
+                    height: 0;
+                    background: conic-gradient(from 180deg at 50% 50%, #F0F1F2 -26.25deg, #F2D5DB 50.63deg, rgba(102, 93, 115, 0.84375) 127.5deg, rgba(120, 119, 140, 0.609447) 210deg, #B0C1D9 279.38deg, #F0F1F2 333.75deg, #F2D5DB 410.62deg);
+
+                    position: absolute;
+                    left: 50%;
+                    top: 50%;
+                    translate: -50% -50%;
+                }
+
+                // 按钮轮盘
+                .btn-wheel {
+                    .btn {
+                        width: 0;
+                        height: 0;
+                        rotate: 180deg;
+
+                        position: absolute;
+                        left: 50%;
+                        top: 50%;
+                        translate: -50% -50%;
+
+                        display: flex;
+                        flex-direction: column;
+                        align-items: center;
+
+
+                        &:nth-child(1) {
+                            rotate: 0deg;
+                        }
+
+                        .btn-icon {
+                            width: 0px;
+                            height: 0px;
+                        }
+
+                        .btn-name {
+                            font-size: 0px;
+                            line-height: 0px;
+                        }
+                    }
+                }
+            }
+
+            // 激活时的环型菜单
+            &.activated {
+
+                // 外圈边框
+                .menu-border {
+                    width: 320px;
+                    height: 320px;
+                    border-radius: 50%;
+                    background: conic-gradient(from 180deg at 50% 50%, #F0F1F2 -26.25deg, #F2D5DB 50.63deg, rgba(102, 93, 115, 0.84375) 127.5deg, rgba(120, 119, 140, 0.609447) 210deg, #B0C1D9 279.38deg, #F0F1F2 333.75deg, #F2D5DB 410.62deg);
+                    position: absolute;
+                    left: 50%;
+                    top: 50%;
+                    translate: -50% -50%;
+                }
+
+                // 按钮轮盘
+                .btn-wheel {
+                    width: 300px;
+                    height: 300px;
+                    background: #F7F8FC;
+                    border-radius: 50%;
+
+                    position: absolute;
+                    left: 50%;
+                    top: 50%;
+                    translate: -50% -50%;
+
+                    // 按钮
+                    .btn {
+                        width: 60px;
+                        height: 85px;
+
+                        position: absolute;
+                        left: 50%;
+                        top: 50%;
+                        translate: -50% 50px;
+
+                        display: flex;
+                        flex-direction: column;
+                        align-items: center;
+
+                        .btn-icon {
+                            width: 55px;
+                            height: 55px;
+                        }
+
+                        .btn-name {
+                            font-weight: 400;
+                            font-size: 30px;
+                            line-height: 30px;
+                        }
+
+                        // 将其中一个按钮旋转到对侧
+                        &:nth-child(1) {
+                            transform-origin: center -50px;
+                            // translate: -50% -100px;
+                            rotate: 180deg;
+                        }
+                    }
+                }
+            }
+        }
+
+        // 中心按钮
+        .center-btn {
+
+            // 按钮背景
+            .center-btn-bg {
+                width: 33px;
+                height: 33px;
+
+                background: #D3D3D3;
+                border-radius: 5px;
+                transform: rotate(-45deg);
+            }
+
+            // 未激活状态
+            &.unactivated {
+
+                // 打开菜单按钮
+                .btn-open {
+                    // z-index: 1;
+                    width: 25px;
+                    height: 25px;
+
+                    position: absolute;
+                    left: 50%;
+                    top: 50%;
+                    translate: -50% -50%;
+
+                    .rect {
+                        width: 15px;
+                        height: 3px;
+                        border-radius: 5px;
+                        background: #000000;
+
+                        position: absolute;
+                        left: calc(50% - 15px/2);
+                        top: calc(50% - 1.5px);
+
+                        &:nth-child(1) {
+                            translate: 0 5.5px;
+                        }
+
+                        &:nth-child(3) {
+                            translate: 0 -5.5px;
+                        }
+                    }
+                }
+
+                // 关闭菜单按钮
+                .btn-close {
+                    display: none;
+                    // z-index: -1;
+
+                    position: absolute;
+                    left: 50%;
+                    top: 50%;
+
+                    .rect {
+                        width: 0;
+                        height: 0;
+                        rotate: 180deg;
+                    }
+                }
+            }
+
+            // 激活状态
+            &.activated {
+
+                // 打开菜单按钮
+                .btn-open {
+                    display: none;
+
+                    position: absolute;
+                    left: 50%;
+                    top: 50%;
+
+                    .rect {
+                        width: 0;
+                        height: 0;
+                        rotate: 180deg;
+                    }
+                }
+
+                // 关闭菜单按钮
+                .btn-close {
+                    // z-index: 1;
+                    width: 25px;
+                    height: 25px;
+
+                    position: absolute;
+                    left: calc(50% - 25px/2);
+                    top: calc(50% - 25px/2);
+
+                    .rect {
+                        position: absolute;
+                        width: 23px;
+                        height: 3.5px;
+                        border-radius: 5px;
+                        left: calc(50% - 23px/2);
+                        top: calc(50% - 3.5px/2);
+
+                        background: #000000;
+
+                        &:nth-child(1) {
+                            rotate: 45deg;
+                        }
+
+                        &:nth-child(2) {
+                            rotate: -45deg;
+                        }
+                    }
+                }
+            }
+        }
+    }
+</style>
--- subpkg/test/test.vue
+++ subpkg/test/test.vue
@@ -1,172 +1,182 @@
 <template>
-  <view class="container">
-    <view class="game-board">
-      <test-player class="player player1" plyaerName="player1"></test-player>
-      <test-player class="player player2" plyaerName="player2"></test-player>
+    <view class="container">
+        <view class="game-board">
+            <test-player class="player player1" plyaerName="player1"></test-player>
+            <test-player class="player player2" plyaerName="player2"></test-player>
+            <!-- 中心菜单 -->
+            <sa-menu class="menu"></sa-menu>
+        </view>
     </view>
-  </view>
 </template>
 
 <script setup lang="ts">
-  // import {
-  //   mapState,
-  //   mapMutations
-  // } from 'vuex'
-  // // 导入并混入添加分享功能 
-  // import {
-  //   showShareMenu
-  // } from '@/common/showShareMenu.js'
-  // export default {
-  //   mixins: [showShareMenu],
-  //   data() {
-  //     return {
-  //       toShowHelp: true
-  //     };
-  //   },
-  //   computed: {},
-  //   methods: {
-  //     ...mapMutations('m_sa', ['resetState', 'getFromStorage']),
-  //     // 显示帮助
-  //     showHelp() {
-  //       this.$refs.popup.open()
-  //     },
-  //     // 重置相关数值
-  //     reset() {
-  //       uni.showModal({
-  //         content: '是否重置所有数值？',
-  //         success: (res) => { // success是只要接口调用成功都会执行
-  //           if (res.confirm) { // res.confirm才是点击确认才执行
-  //             this.resetState()
-  //           }
-  //         }
-  //       })
-  //     }
-  //   },
-  //   // 加载时从本地存储获取数据
-  //   onLoad() {
-  //     // 获取游戏面板数据
-  //     this.getFromStorage()
-  //     // 获取是否显示帮助参数
-  //     this.toShowHelp = uni.getStorageSync('sa_toShowHelp').length != 0 ?
-  //       JSON.parse(uni.getStorageSync('sa_toShowHelp')) :
-  //       this.toShowHelp
-  //     // 判断是否显示帮助
-  //     if (this.toShowHelp) {
-  //       this.showHelp()
-  //       uni.setStorageSync('sa_toShowHelp', JSON.stringify(false))
-  //     }
-  //   },
-  //   // 点击分享按钮时的事件
-  //   onShareAppMessage(res) {
+    // import {
+    //   mapState,
+    //   mapMutations
+    // } from 'vuex'
+    // // 导入并混入添加分享功能 
+    // import {
+    //   showShareMenu
+    // } from '@/common/showShareMenu.js'
+    // export default {
+    //   mixins: [showShareMenu],
+    //   data() {
+    //     return {
+    //       toShowHelp: true
+    //     };
+    //   },
+    //   computed: {},
+    //   methods: {
+    //     ...mapMutations('m_sa', ['resetState', 'getFromStorage']),
+    //     // 显示帮助
+    //     showHelp() {
+    //       this.$refs.popup.open()
+    //     },
+    //     // 重置相关数值
+    //     reset() {
+    //       uni.showModal({
+    //         content: '是否重置所有数值？',
+    //         success: (res) => { // success是只要接口调用成功都会执行
+    //           if (res.confirm) { // res.confirm才是点击确认才执行
+    //             this.resetState()
+    //           }
+    //         }
+    //       })
+    //     }
+    //   },
+    //   // 加载时从本地存储获取数据
+    //   onLoad() {
+    //     // 获取游戏面板数据
+    //     this.getFromStorage()
+    //     // 获取是否显示帮助参数
+    //     this.toShowHelp = uni.getStorageSync('sa_toShowHelp').length != 0 ?
+    //       JSON.parse(uni.getStorageSync('sa_toShowHelp')) :
+    //       this.toShowHelp
+    //     // 判断是否显示帮助
+    //     if (this.toShowHelp) {
+    //       this.showHelp()
+    //       uni.setStorageSync('sa_toShowHelp', JSON.stringify(false))
+    //     }
+    //   },
+    //   // 点击分享按钮时的事件
+    //   onShareAppMessage(res) {
 
-  //   }
-  // }
+    //   }
+    // }
 </script>
 
 <style scoped lang="scss">
-  .container {
-    height: 100vh;
-    width: 100vw;
-    position: fixed;
-    background: linear-gradient(180.05deg, #B0C1D9 -2.17%, #F2D5DB 99.95%);
+    .container {
+        height: 100vh;
+        width: 100vw;
+        position: fixed;
+        background: linear-gradient(180.05deg, #B0C1D9 -2.17%, #F2D5DB 99.95%);
 
-    padding: 10px 0 36px;
+        padding: 10px 0 36px;
 
-    // 帮助说明
-    .help-container {
-      // position: absolute;
-      // left: 50%;
-      transform: translateX(50px) translateY(20px);
-      width: calc(100% - 100px);
-
-
-      .help {
-        color: #fff;
-        font-size: 18px;
-        text-align: justify;
-
-        view {
-          margin-top: 10px;
-          align-items: center;
-
-          .bold {
-            font-weight: bold;
-          }
-
-          text {
-            line-height: 100%;
-
-          }
-
-          .reset-icon {
-            display: inline-block;
-          }
+        // 帮助说明
+        .help-container {
+            // position: absolute;
+            // left: 50%;
+            transform: translateX(50px) translateY(20px);
+            width: calc(100% - 100px);
 
 
+            .help {
+                color: #fff;
+                font-size: 18px;
+                text-align: justify;
+
+                view {
+                    margin-top: 10px;
+                    align-items: center;
+
+                    .bold {
+                        font-weight: bold;
+                    }
+
+                    text {
+                        line-height: 100%;
+
+                    }
+
+                    .reset-icon {
+                        display: inline-block;
+                    }
+
+
+                }
+
+                // 反馈说明
+                .feedback {
+                    margin-top: 20px;
+                    display: flex;
+                    justify-content: center;
+                    flex-wrap: wrap;
+
+                    text {
+                        font-size: 15px;
+                    }
+
+                    button {
+                        margin-top: 5px;
+                    }
+                }
+
+                // 关闭提示
+                .close-tip {
+                    margin-top: 20px;
+                    display: flex;
+                    justify-content: center;
+
+                    text {}
+                }
+
+
+            }
         }
 
-        // 反馈说明
-        .feedback {
-          margin-top: 20px;
-          display: flex;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          text {
-            font-size: 15px;
-          }
-
-          button {
-            margin-top: 5px;
-          }
-        }
-
-        // 关闭提示
-        .close-tip {
-          margin-top: 20px;
-          display: flex;
-          justify-content: center;
-
-          text {}
+        // 重置按钮
+        .reset {
+            position: absolute;
+            top: 50%;
+            left: -10px;
+            transform: translateY(-50%);
+            z-index: 999999;
         }
 
 
-      }
+        // 游戏版图
+        .game-board {
+            display: flex;
+            flex-direction: column;
+            height: 98vh;
+            width: 100vw;
+
+            .player:nth-child(1) {
+                margin-bottom: 8px;
+                // position: absolute;
+
+            }
+
+            .player1 {
+                transform: rotate(180deg);
+                // left: 0;
+                // top: 4.97vh;
+            }
+
+            .player2 {
+                left: 0%;
+                // top: 48.27vh;
+            }
+        }
+
+        // 中心菜单
+        .menu {
+            position: absolute;
+            left: 50%;
+            top: 46.3%;
+            translate: -50% -50%;
+        }
     }
-
-    // 重置按钮
-    .reset {
-      position: absolute;
-      top: 50%;
-      left: -10px;
-      transform: translateY(-50%);
-      z-index: 999999;
-    }
-
-    .menu {}
-
-    // 游戏版图
-    .game-board {
-      display: flex;
-      flex-direction: column;
-      height: 98vh;
-      width: 100vw;
-
-      .player:nth-child(1) {
-        margin-bottom: 8px;
-        // position: absolute;
-      }
-
-      .player1 {
-        transform: rotate(180deg);
-        // left: 0;
-        // top: 4.97vh;
-      }
-
-      .player2 {
-        left: 0%;
-        // top: 48.27vh;
-      }
-    }
-  }
 </style>
