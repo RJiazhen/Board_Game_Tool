4634d969856ac98da874d5a09e37e7b8eaf6dc2f
RJiazhen <jiazhen.ruan@outlook.com>
2023-01-12 22:12:42 +0800

    feat: 完成樱花总数提示组件

--- components/sa-token-tip/sa-token-tip.vue
+++ components/sa-token-tip/sa-token-tip.vue
@@ -1,21 +1,56 @@
 <template>
     <view class="token-tip">
         <image class="token-icon" src="../../static/sakura_token_icon.svg" mode=""></image>
-        <view class="token-count">
-            +1
+        <view class="token-count" :class="tokenCountStyle">
+            <text v-if="tokenDifference>0">+</text>
+            <text v-if="tokenDifference==0">&nbsp;</text>
+            <text>{{tokenDifference}}</text>
         </view>
     </view>
 </template>
 
-<script>
-    export default {
-        name: "sa-token-tip",
-        data() {
-            return {
+<script setup lang="ts">
+    import {
+        computed
+    } from "vue";
 
-            };
+    import {
+        useSakuraArms
+    } from "@/store/sakuraArms"
+    import {
+        onReady
+    } from '@dcloudio/uni-app'
+    const sakuraArms = useSakuraArms()
+
+    // 当前面板的token数量和理论数量（36）之间的差值
+    const tokenDifference = computed(() => {
+        let count = 0
+        // 公共区域token数量计算
+        for (let i in sakuraArms.currentState.shared) {
+            count += sakuraArms.currentState.shared[i].count
         }
-    }
+        // player1区域token数量计算
+        for (let i in sakuraArms.currentState.player1) {
+            count += sakuraArms.currentState.player1[i].count
+        }
+        // player2区域token数量计算
+        for (let i in sakuraArms.currentState.player2) {
+            count += sakuraArms.currentState.player2[i].count
+        }
+        return 36 - count
+    })
+
+
+    // 数字样式
+    const tokenCountStyle = computed(() => {
+        if (tokenDifference.value > 0) {
+            return 'add-count'
+        }
+        if (tokenDifference.value < 0) {
+            return 'minus-count'
+        }
+        return ''
+    })
 </script>
 
 <style scoped lang="scss">
@@ -50,7 +85,16 @@
             font-size: 25px;
             line-height: 40px;
 
-            color: #CF6379;
+            color: #C7CDD0;
+
+            &.add-count {
+                color: #CF6379;
+
+            }
+
+            &.minus-count {
+                color: #23c3db;
+            }
         }
     }
 </style>
