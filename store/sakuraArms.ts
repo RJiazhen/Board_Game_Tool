5e9ffc88d5798639e39ab4324c606ad6e443d8bf
RJiazhen <jiazhen.ruan@outlook.com>
2023-01-26 21:17:23 +0800

    chore: 格式化代码以及删除调试用的console.log代码

--- store/sakuraArms.ts
+++ store/sakuraArms.ts
@@ -1,8 +1,6 @@
-import {
-    defineStore
-} from 'pinia';
-import { computed, ref, toDisplayString } from 'vue'
-import _ from "lodash"
+import { defineStore } from 'pinia';
+import { computed, ref, toDisplayString } from 'vue';
+import _ from 'lodash';
 
 export const useSakuraArms = defineStore('sakuraArms', () => {
     // 初始状态 考虑到未来的功能扩展，仍保留回合数，回合玩家等数据
@@ -16,11 +14,11 @@ export const useSakuraArms = defineStore('sakuraArms', () => {
         shared: {
             distance: {
                 count: 10,
-                limit: 10,
+                limit: 10
             },
             shadow: {
                 count: 0,
-                limit: null,
+                limit: null
             }
         },
         // 玩家一
@@ -28,248 +26,244 @@ export const useSakuraArms = defineStore('sakuraArms', () => {
             // 装
             aura: {
                 count: 3,
-                limit: 5,
+                limit: 5
             },
             // 命
             life: {
                 count: 10,
-                limit: null,
+                limit: null
             },
             // 气
             flare: {
                 count: 0,
-                limit: null,
+                limit: null
             },
             // 付与牌
             enhancementA: {
-                order: "A",
+                order: 'A',
                 count: 0,
                 show: true,
-                limit: null,
+                limit: null
             },
             enhancementB: {
-                order: "B",
+                order: 'B',
                 count: 0,
                 show: true,
-                limit: null,
+                limit: null
             },
             enhancementC: {
-                order: "C",
+                order: 'C',
                 count: 0,
                 show: true,
-                limit: null,
+                limit: null
             },
             enhancementD: {
-                order: "D",
+                order: 'D',
                 count: 0,
                 show: false,
-                limit: null,
+                limit: null
             },
             enhancementE: {
-                order: "E",
+                order: 'E',
                 count: 0,
                 show: false,
-                limit: null,
+                limit: null
             },
             enhancementF: {
-                order: "F",
+                order: 'F',
                 count: 0,
                 show: false,
-                limit: null,
+                limit: null
             },
             enhancementG: {
-                order: "G",
+                order: 'G',
                 count: 0,
                 show: false,
-                limit: null,
-            },
+                limit: null
+            }
         },
         // 玩家二
         player2: {
             // 装
             aura: {
                 count: 3,
-                limit: 5,
+                limit: 5
             },
             // 命
             life: {
                 count: 10,
-                limit: null,
+                limit: null
             },
             // 气
             flare: {
                 count: 0,
-                limit: null,
+                limit: null
             },
             // 付与牌
             enhancementA: {
-                order: "A",
+                order: 'A',
                 count: 0,
                 show: true,
-                limit: null,
+                limit: null
             },
             enhancementB: {
-                order: "B",
+                order: 'B',
                 count: 0,
                 show: true,
-                limit: null,
+                limit: null
             },
             enhancementC: {
-                order: "C",
+                order: 'C',
                 count: 0,
                 show: true,
-                limit: null,
+                limit: null
             },
             enhancementD: {
-                order: "D",
+                order: 'D',
                 count: 0,
                 show: false,
-                limit: null,
+                limit: null
             },
             enhancementE: {
-                order: "E",
+                order: 'E',
                 count: 0,
                 show: false,
-                limit: null,
+                limit: null
             },
             enhancementF: {
-                order: "F",
+                order: 'F',
                 count: 0,
                 show: false,
-                limit: null,
+                limit: null
             },
             enhancementG: {
-                order: "G",
+                order: 'G',
                 count: 0,
                 show: false,
-                limit: null,
-            },
+                limit: null
+            }
         }
-    }
+    };
 
     // 当前状态
-    const currentState = ref({})
+    const currentState = ref({});
 
     // 恢复初始状态
     const resetState = () => {
         // 清除本地存储数据
         uni.removeStorage({
             key: 'sakuraArms',
-            success() { }
-        })
+            success() {}
+        });
 
         // 将当前状态重置回初始状态
-        currentState.value = JSON.parse(JSON.stringify(initialState))
-    }
+        currentState.value = JSON.parse(JSON.stringify(initialState));
+    };
 
     // 把数据存储到本地
     const saveToStorage = () => {
-        uni.setStorageSync('sakuraArms', JSON.stringify(currentState.value))
-    }
+        uni.setStorageSync('sakuraArms', JSON.stringify(currentState.value));
+    };
 
     // 从本地获取数据，如果无则设定为默认值
     const getFromStorage = () => {
-        currentState.value = uni.getStorageSync('sakuraArms').length != 0 ? JSON.parse(uni.getStorageSync('sakuraArms')) : JSON.parse(JSON.stringify(initialState))
-    }
+        currentState.value = uni.getStorageSync('sakuraArms').length != 0 ? JSON.parse(uni.getStorageSync('sakuraArms')) : JSON.parse(JSON.stringify(initialState));
+    };
 
     // 减token数量，无法再减则返回false，否则返回true
     const minusToken = (primaryIndex: string, areaName: string) => {
         // 如果当前token数量小于等于零，则不修改
         if (_.get(currentState.value, `${primaryIndex}.${areaName}.count`) <= 0) {
-            return false
+            return false;
         }
-        _.update(currentState.value, `${primaryIndex}.${areaName}.count`, (n) => n - 1)
+        _.update(currentState.value, `${primaryIndex}.${areaName}.count`, n => n - 1);
         // 修改数据后存储到本地
-        saveToStorage()
+        saveToStorage();
 
-        return true
-    }
+        return true;
+    };
 
     // 加token数量，无法再添加则返回false，否则返回true
     const addToken = (primaryIndex: string, areaName: string) => {
-        let limit = _.get(currentState.value, `${primaryIndex}.${areaName}.limit`)
+        let limit = _.get(currentState.value, `${primaryIndex}.${areaName}.limit`);
         // 如果有上限且当前token数量已达/超过上限，则不变化
         if (limit != null) {
             if (_.get(currentState.value, `${primaryIndex}.${areaName}.count`) >= limit) {
-                return false
+                return false;
             }
         }
-        _.update(currentState.value, `${primaryIndex}.${areaName}.count`, (n) => n + 1)
+        _.update(currentState.value, `${primaryIndex}.${areaName}.count`, n => n + 1);
         // 修改数据后存储到本地
-        saveToStorage()
+        saveToStorage();
 
-        return true
-    }
+        return true;
+    };
 
     // 显示付与牌
     const addEnhanCard = (primaryIndex: string) => {
         for (let i in currentState.value[primaryIndex]) {
             // 将编号最靠前的、show为false的付与牌设置为显示
             if (i.indexOf('enhancement') != -1 && !currentState.value[primaryIndex][i]['show']) {
-                currentState.value[primaryIndex][i]['show'] = true
-                return
+                currentState.value[primaryIndex][i]['show'] = true;
+                return;
             }
         }
         // 修改数值后存储到本地
-        saveToStorage()
-    }
+        saveToStorage();
+    };
 
     // 全部付与牌减一
     const removeAllEnhanCardToken = (primaryIndex: string) => {
         for (let i in currentState.value[primaryIndex]) {
             // 全部count大于0、且show为true的付与牌的token数量减一
             if (i.indexOf('enhancement') != -1 && currentState.value[primaryIndex][i]['show'] && currentState.value[primaryIndex][i]['count'] > 0) {
-                currentState.value[primaryIndex][i]['count'] -= 1
+                currentState.value[primaryIndex][i]['count'] -= 1;
             }
         }
         // 修改数值后存储到本地
-        saveToStorage()
-    }
+        saveToStorage();
+    };
     // 理论总的token数量
-    const totalTokenCount = ref(36)
+    const totalTokenCount = ref(36);
 
     // 当前token总数
     const currentTotalTokenCount = computed(() => {
-        let count = 0
+        let count = 0;
         // 公共区域token数量计算
         for (let i in currentState.value.shared) {
-            count += currentState.value.shared[i].count
+            count += currentState.value.shared[i].count;
         }
         // player1区域token数量计算
         for (let i in currentState.value.player1) {
-            count += currentState.value.player1[i].count
+            count += currentState.value.player1[i].count;
         }
         // player2区域token数量计算
         for (let i in currentState.value.player2) {
-            count += currentState.value.player2[i].count
+            count += currentState.value.player2[i].count;
         }
-        return count
-    })
+        return count;
+    });
 
     // 当前token差值
     const tokenDifference = computed(() => {
-        return currentTotalTokenCount.value - totalTokenCount.value
-    })
+        return currentTotalTokenCount.value - totalTokenCount.value;
+    });
 
     // 使用「虚」token数量来抵消差值
     const offsetTokenDifference = () => {
-        console.log(currentTotalTokenCount.value);
-        console.log(tokenDifference.value);
-
         // 当差值为零时，不进行任何操作
         if (tokenDifference.value === 0) {
-            return
+            return;
         }
         // 当「虚」中token数量大于等于差值（无论正负数），虚中token数量直接减去差值
         if (currentState.value.shared.shadow.count >= tokenDifference.value) {
-            currentState.value.shared.shadow.count -= tokenDifference.value
+            currentState.value.shared.shadow.count -= tokenDifference.value;
         }
         // 当「虚」中token数量小于差值（无论正负数），将虚中token清零
         if (currentState.value.shared.shadow.count < tokenDifference.value) {
-            currentState.value.shared.shadow.count = 0
+            currentState.value.shared.shadow.count = 0;
         }
-    }
+    };
 
-
-    return { resetState, currentState, saveToStorage, getFromStorage, minusToken, addToken, addEnhanCard, removeAllEnhanCardToken, tokenDifference, offsetTokenDifference }
-})
+    return { resetState, currentState, saveToStorage, getFromStorage, minusToken, addToken, addEnhanCard, removeAllEnhanCardToken, tokenDifference, offsetTokenDifference };
+});
